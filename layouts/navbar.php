20ea758000dd9d65a122744b799aeb1f32a06699
thura93 <thura93.2022@gmail.com>
2022-10-10 16:28:58 +0630

    nav on fa-user add

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -18,7 +18,7 @@
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <?php if($auth->role_id == 4 || $auth->role_id == 5) : ?>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="users.php">User Management</a>
+            <a class="nav-link active" aria-current="page" href="users.php"> <i class="fa fa-user"></i> User Management</a>
           </li>
         <?php endif; ?>
         <?php if($auth == false) : ?>
